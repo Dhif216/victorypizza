ed095c1ba98f38ee2c7277cbd3f21cc4dfcad8bd
Dhif216 <dhif_mouadh@hotmail.fr>
2025-12-06 11:00:47 +0200

    Deploy: Darker theme + mobile responsive

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-
-  import { createRoot } from "react-dom/client";
-  import App from "./App.tsx";
-  import "./index.css";
-  import "./styles/app.css";
-  import "./styles/overrides.css";
-
-  createRoot(document.getElementById("root")!).render(<App />);
-  
