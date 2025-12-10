ae8f503f85c85bec7ff2b4154df9e28d43ae5db5
Dhif <dhif_mouadh@hotmail.fr>
2025-12-10 04:17:22 +0200

    DISABLE overrides.css - it was forcing all orange/amber to green

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,9 @@
 import { createRoot } from "react-dom/client";
-  import App from "./App.tsx";
-  import "./index.css";
-  import "./styles/app.css";
-  import "./styles/overrides.css";
-  import { CartProvider } from "./context/CartContext";
-
-  createRoot(document.getElementById("root")!).render(
+import App from "./App.tsx";
+import "./index.css";
+import "./styles/app.css";
+// import "./styles/overrides.css"; // DISABLED - was forcing green colors
+import { CartProvider } from "./context/CartContext";  createRoot(document.getElementById("root")!).render(
     <CartProvider>
       <App />
     </CartProvider>
