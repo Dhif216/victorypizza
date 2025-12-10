a23eafb1b370401299c29bb688a0fd8130a9bb6b
Dhif <dhif_mouadh@hotmail.fr>
2025-12-10 03:49:45 +0200

    Restore overrides.css import to fix styling

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
   import App from "./App.tsx";
   import "./index.css";
   import "./styles/app.css";
+  import "./styles/overrides.css";
   import { CartProvider } from "./context/CartContext";
 
   createRoot(document.getElementById("root")!).render(
